8524a15e885d17684fcdf8e484450eeae76e2fd1
stanmoriasi <stanmoriasi@yahoo.com>
2025-01-07 06:56:42 -0700

    override printDetails and implement wheelie in motorbike.ts

--- src/classes/Motorbike.ts
+++ src/classes/Motorbike.ts
@@ -3,7 +3,7 @@ import Vehicle from './Vehicle.js';
 import Wheel from './Wheel.js';
 
 // TODO: The Motorbike class should extend the Vehicle class
-class Motorbike {
+class Motorbike  extends Vehicle {
   // TODO: Declare properties of the Motorbike class
   // TODO: The properties should include vin, color, make, model, year, weight, top speed, and wheels
   // TODO: The types should be as follows: vin (string), color (string), make (string), model (string), year (number), weight (number), topSpeed (number), wheels (Wheel[])
@@ -17,6 +17,7 @@ class Motorbike {
   wheels: Wheel[];
   
   constructor(vin: string, color: string, make: string, model: string, year: number, weight: number, topSpeed: number, wheels: Wheel[]) {
+    super();
     this.vin = vin;
     this.color = color;
     this.make = make;
@@ -25,6 +26,7 @@ class Motorbike {
     this.weight = weight;
     this.topSpeed = topSpeed;
     this.wheels = wheels;
+    
   }
 
   // TODO: Create a constructor that accepts the properties of the Motorbike class
@@ -34,11 +36,25 @@ class Motorbike {
 
   // TODO: Implement the wheelie method
     // TODO: The method should log the message "Motorbike [make] [model] is doing a wheelie!"
+  wheelie(): void {
+    console.log(`Motorbike ${this.make} ${this.model} is doing a wheelie!`);
+  }
 
   // TODO: Override the printDetails method from the Vehicle class
   // TODO: The method should call the printDetails method of the parent class
   // TODO: The method should log the details of the Motorbike
   // TODO: The details should include the VIN, make, model, year, weight, top speed, color, and wheels
+  override printDetails(): void {
+    super.printDetails();
+    console.log(`VIN: ${this.vin}`);
+    console.log(`Make: ${this.make}`);
+    console.log(`Model: ${this.model}`);
+    console.log(`Year: ${this.year}`);
+    console.log(`Weight: ${this.weight}`);
+    console.log(`Top Speed: ${this.topSpeed}`);
+    console.log(`Color: ${this.color}`);
+    console.log(`Wheels: ${this.wheels}`);
+  }
 }
 
 // Export the Motorbike class as the default export
